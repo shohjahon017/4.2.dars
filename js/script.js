8910c3be7457da557f38d6cb8ce793b12692ac57
shohjhahon017 <shohjahonsoilov1@gmail.com>
2024-07-18 17:54:37 +0500

    ..

--- js/script.js
+++ js/script.js
@@ -65,12 +65,214 @@
 // console.log(positive, negative);
 
 //if 6-masala
-let num1 = +prompt("1-sonni kiriting");
-let num2 = +prompt("2-sonni kiriting");
-let max = 0;
-if (num1 > num2) {
-  max = num1;
+// let num1 = +prompt("1-sonni kiriting");
+// let num2 = +prompt("2-sonni kiriting");
+// let max = 0;
+// if (num1 > num2) {
+//   max = num1;
+// } else {
+//   max = num2;
+// }
+// console.log(max);
+
+// if 7-masala
+let a = 5;
+let b = 7;
+let res;
+if (a > b) {
+  res = 2;
 } else {
-  max = num2;
+  res = 1;
 }
-console.log(max);
+console.log(res);
+
+//if 8-masala
+// let num1 = +prompt("1-sonni kiriting");
+// let num2 = +prompt("2-sonni kiriting");
+// let max;
+// let min;
+// if (num1 > num2) {
+//   max = num1;
+//   min = num2;
+// } else {
+//   max = num2;
+//   min = num1;
+// }
+// console.log(max, min);
+
+//if 9-masala
+// let a = +prompt("a-sonni kiriting");
+// let b = +prompt("b-sonni kiriting");
+// if (b > a) {
+//   a = b;
+// } else {
+//   b = a;
+// }
+// console.log(a, b);
+
+//if 10-masala
+// let a = +prompt("a-sonni kiriting");
+// let b = +prompt("b-sonni kiriting");
+// let sum = 0;
+// if (a != b) {
+//   sum = a + b;
+// } else {
+//   sum = 0;
+// }
+// console.log(sum, a, b);
+
+//if 11-masala
+// let a = +prompt("a-sonni kiriting");
+// let b = +prompt("b-sonni kiriting");
+// let max;
+// if (a != b) {
+//   if (a > b) {
+//     max = a;
+//   } else {
+//     max = b;
+//   }
+// } else {
+//   max = 0;
+// }
+// console.log(max, a, b);
+
+//if 12-masala
+// let num1 = +prompt("1-sonni kiriting");
+// let num2 = +prompt("2-sonni kiriting");
+// let num3 = +prompt("3-sonni kiriting");
+// let min;
+// if (num1 > num2) {
+//   min = num2;
+// } else {
+//   min = num1;
+// }
+// if (min > c) {
+//   min = c;
+// }
+// console.log(min);
+
+//if 14-masala
+// let num1 = +prompt("1-sonni kiriting");
+// let num2 = +prompt("2-sonni kiriting");
+// let num3 = +prompt("3-sonni kiriting");
+// let min;
+// let max;
+// if (num1 > num2) {
+//   max = num1;
+//   min = num2;
+// } else {
+//   max = num2;
+//   min = num1;
+// }
+// if (min > c) {
+//   min = c;
+// } else {
+//   max = c;
+// }
+// console.log(max, min);
+
+// if 15-masala
+// let a = 5;
+// let b = 7;
+// let c = 3;
+
+// let min;
+// let max;
+
+// if (a < b) {
+//   min = a;
+//   max = b;
+// } else {
+//   min = b;
+//   max = a;
+// }
+// if (min > c) {
+//   min = c;
+// }
+// if (max < c) {
+//   max = c;
+// }
+// let res = a + b + c - (max - min);
+// let res1 = a + b + c - (max - min) + max;
+// console.log(res, res1);
+
+//boolean 1-masala
+// let A = +prompt("A sonni kiritng");
+// if (A > 0) {
+//   console.log("A soni musbat");
+// }
+
+//boolean 2-masala
+// let A = +prompt("A sonni kiritng");
+// if (A < 0) {
+//   console.log("A soni toq son");
+// }
+
+// boolean 3-masala
+// let A = +prompt("A sonni kiritng");
+// if (A % 2 == 0) {
+//   console.log("A soni juft son");
+// }
+
+//boolean 4-masala
+// let A = +prompt("A sonni kiritng");
+// let B = +prompt("B sonni kiriting");
+// if (A > 2 && B <= 3) {
+//   console.log("Rost");
+// }
+
+//boolean 5-masala
+// let a = +prompt("a ni qiymatini kiriting");
+// let b = +prompt("b ni qiymatini kiriting");
+// if (a <= 0 || b < -2) {
+//   console.log("rost");
+// } else {
+//   console.log("yolg`on");
+// }
+
+// boolean 6-masala
+// let a = +prompt("a ni qiymatini kiriting");
+// let b = +prompt("b ni qiymatini kiriting");
+// let c = +prompt("c ni qiymatini kiriting");
+// if (a <= b <=c) {
+//   console.log("rost");
+// } else {
+//   console.log("yolg`on");
+// }
+
+//boolean 7-masala
+// let a = +prompt("a ni qiymatini kiriting");
+// let b = +prompt("b ni qiymatini kiriting");
+// let c = +prompt("c ni qiymatini kiriting");
+// if (a < b && b<c) {
+//   console.log("rost");
+// } else {
+//   console.log("yolg`on");
+// }
+
+// boolean 8-masala
+// let a = +prompt("a ni qiymatini kiriting");
+// let b = +prompt("b ni qiymatini kiriting");
+// if (a % 2 == 0 && b % 2 == 0) {
+//   console.log(" a va b soni toq son");
+// } else {
+//   console.log("toq son emas");
+// }
+
+// boolean 9-masala
+// let a = +prompt("a ni qiymatini kiriting");
+// let b = +prompt("b ni qiymatini kiriting");
+// if (a % 2 == 1 || b % 2 == 1) {
+//   console.log(" a va b soni toq son");
+// } else {
+//   console.log("toq son emas");
+// }
+
+// boolean 10-masala
+// let a = +prompt("a ni qiymatini kiriting");
+// let b = +prompt("b ni qiymatini kiriting");
+// if ((a % 2 == 0 && b % 2 == 1) || (a % 2 == 1 && b % 2 == 0)) {
+//   console.log(" a va b sonlarining faqat bittasi toq son");
+// } else {
+//   console.log("toq son emas");
+// }
