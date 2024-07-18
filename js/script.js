d9490beed20a32a312bb634a5b4c7a8e4e32fd98
shohjhahon017 <shohjahonsoilov1@gmail.com>
2024-07-18 15:29:37 +0500

    .

---
+++ js/script.js
@@ -0,0 +1,76 @@
+// if 1-masala
+// let num = +prompt("sonni kiriting");
+// if (num > 0) {
+//   num++;
+// }
+// console.log(num);
+
+// if 2-masala
+// let num = +prompt("sonni kiriting");
+// if (num > 0) {
+//   num++;
+// } else {
+//   num -= 2;
+// }
+// console.log(num);
+
+// if 3-masala
+// let num = +prompt("sonni kiriting");
+// if (num > 0) {
+//   num++;
+// } else if (num == 0) {
+//   num = 10;
+// } else {
+//   num -= 2;
+// }
+// console.log(num);
+
+//if 4-masala
+// let num1 = +prompt("1-sonni kiriting");
+// let num2 = +prompt("2-sonni kiriting");
+// let num3 = +prompt("3-sonni kiriting");
+// let positive = 0;
+// if (num1 > 0) {
+//   positive++;
+// }
+// if (num2 > 0) {
+//   positive++;
+// }
+// if (num3 > 0) {
+//   positive++;
+// }
+// console.log(positive);
+
+//if 5-masala
+// let num1 = +prompt("1-sonni kiriting");
+// let num2 = +prompt("2-sonni kiriting");
+// let num3 = +prompt("3-sonni kiriting");
+// let positive = 0;
+// let negative = 0;
+// if (num1 > 0) {
+//   positive++;
+// } else if (num1 < 0) {
+//   negative++;
+// }
+// if (num2 > 0) {
+//   positive++;
+// } else if (num2 < 0) {
+//   negative++;
+// }
+// if (num3 > 0) {
+//   positive++;
+// } else if (num3 < 0) {
+//   negative++;
+// }
+// console.log(positive, negative);
+
+//if 6-masala
+let num1 = +prompt("1-sonni kiriting");
+let num2 = +prompt("2-sonni kiriting");
+let max = 0;
+if (num1 > num2) {
+  max = num1;
+} else {
+  max = num2;
+}
+console.log(max);
